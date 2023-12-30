f1efde0b677c5453fa387ae888d84509150c76e4
Victor Garcia <victorbenitogr@gmail.com>
2023-12-30 15:44:35 -0600

    R and py files

---
+++ src/OpenHealthData.R
@@ -0,0 +1,36 @@
+# * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# * OpenHealthData
+# * https://github.com/VictorBenitoGR/OpenHealthData
+# * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+# *** PACKAGES *** ------------------------------------------------------------
+
+library(dplyr)       # Data manipulation
+library(ggplot2)     # Data visualization
+library(lubridate)   # Date manipulation
+library(stringr)     # String manipulation
+library(knitr)       # Dynamic reports
+library(rmarkdown)   # Markdown reports
+library(shiny)       # Web applications
+library(plotly)      # Interactive plots
+library(tidyverse)   # Data manipulation and visualization
+
+
+# *** DATA IMPORT *** ----------------------------------------------------------
+
+
+
+# *** APPOINTMENT RESCHEDULING *** --------------------------------------------
+
+
+
+# *** FUNDRAISING CAMPAIGNS *** ------------------------------------------------
+
+
+
+# *** RELATIONSHIP WITH LEGAL GUARDIAN (MINORS) *** ---------------------------
+
+
+
+# *** RECOMMENDABILITY BY AGE AND TREATMENT *** -------------------------------
+
---
+++ src/OpenHealthData_forecast.py
@@ -0,0 +1,20 @@
+# * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# * OpenHealthData
+# * https://github.com/VictorBenitoGR/OpenHealthData
+# * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+# *** PACKAGES *** ------------------------------------------------------------
+
+import numpy as np # Linear algebra
+import pandas as pd # Data processing
+import tensorflow as tf # Machine learning
+from tensorflow import keras # Deep learning
+from sklearn.preprocessing import MinMaxScaler # Data normalization
+from sklearn.metrics import mean_squared_error # Error metric
+
+
+# *** DATA IMPORT *** ---------------------------------------------------------
+
+
+
+# *** FORECASTING *** ---------------------------------------------------------
