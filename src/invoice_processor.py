f99bd85179c23450d354f443ea5190bac12dad87
Victor Garcia <victorbenitogr@gmail.com>
2024-06-10 19:08:09 -0600

    Form Recognizer refactor

--- src/invoice_processor.py
+++ src/invoice_processor.py
@@ -1,48 +1,74 @@
-
 # * Invoice processor
 
-# pip install azure-ai-formrecognizer
-from azure.ai.formrecognizer import FormRecognizerClient
-from azure.core.credentials import AzureKeyCredential
-import pandas as pd
+# *** Libraries ---------------------------------------------------------------
+
 import os
+import pandas as pd
 from dotenv import load_dotenv
+from azure.ai.formrecognizer import DocumentAnalysisClient
+from azure.core.credentials import AzureKeyCredential
+from datetime import datetime
 
-# Cargar las variables de entorno desde el archivo .env
+# *** Environment variables ---------------------------------------------------
+
 load_dotenv()
-endpoint = os.getenv("FORM_RECOGNIZER_ENDPOINT")
-api_key = os.getenv("FORM_RECOGNIZER_API_KEY")
+
+endpoint = os.getenv('FORM_RECOGNIZER_ENDPOINT')
+key = os.getenv('FORM_RECOGNIZER_API_KEY')
+
+# Initialize the DocumentAnalysisClient
+client = DocumentAnalysisClient(
+    endpoint=endpoint, credential=AzureKeyCredential(key))
+
+# *** Functions ---------------------------------------------------------------
+
+def process_invoice(invoice_path):
+    # Open the invoice file
+    with open(invoice_path, "rb") as f:
+        # Analyze the document
+        poller = client.begin_analyze_document("prebuilt-invoice", document=f)
+        result = poller.result()
+
+    # Extract and return the data
+    extracted_data = {}
+    for doc in result.documents:
+        for name, field in doc.fields.items():
+            if field.value_type not in ["list", "dictionary"]:
+                extracted_data[name] = field.content
+    return extracted_data
 
 
-# Crear un cliente de Form Recognizer
-client = FormRecognizerClient(endpoint, AzureKeyCredential(api_key))
+def process_all_invoices(invoice_directory):
+    invoice_files = os.listdir(invoice_directory)
+    all_data = []
 
-# Leer el archivo PDF
-with open("./data/invoices/invoice1.pdf", "rb") as f:
-  invoice = f.read()
+    for invoice_file in invoice_files:
+        invoice_path = os.path.join(invoice_directory, invoice_file)
+        invoice_data = process_invoice(invoice_path)
+        all_data.append(invoice_data)
 
-# Enviar el archivo PDF a Form Recognizer para extraer la información
-poller = client.begin_recognize_invoices(invoice)
-result = poller.result()
+    # Convert the list of data into a pandas DataFrame
+    invoices_df = pd.DataFrame(all_data)
 
-# Crear un DataFrame para almacenar la información
-data = []
+    # Drop the unnecessary columns
+    invoices_df = invoices_df.drop(
+        columns=['CustomerAddressRecipient', 'CustomerId', 'CustomerName'])
 
-# Extraer la información de la factura
-for recognized_invoice in result:
-  invoice_data = {}
-  for name, field in recognized_invoice.fields.items():
-    if name == "Items":
-      for idx, items in enumerate(field.value):
-        item_data = {f"Item_{name}_{idx}_{item_name}": item.value for item_name, item in items.value.items()}
-        invoice_data.update(item_data)
-    else:
-      invoice_data[name] = field.value
-  data.append(invoice_data)
+    # Get the current date and format it
+    current_date = datetime.now()
+    formatted_date = current_date.strftime('%Y-%m')
 
-df = pd.DataFrame(data)
+    # Build the filename with the current date
+    filename = f"./data/invoices_{formatted_date}.csv"
 
-print(df)
+    # Export the DataFrame to CSV
+    invoices_df.to_csv(filename, index=False)
 
-# Guardar el DataFrame como un archivo CSV
-df.to_csv("./data/invoice_data.csv", index=False)
+    return invoices_df
+
+# *** Main script -------------------------------------------------------------
+
+
+invoices_df = process_all_invoices("./data/invoices")
+
+print(invoices_df)
